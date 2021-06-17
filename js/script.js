f5a255f023afdf652b4f947d5ce3d44653b9c57d
Masaki Shuhei <78774117+Shuhei-Masaki@users.noreply.github.com>
2021-06-17 13:10:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,6 +30,18 @@ $(function() {
     dots:true,
     slidesToShow:4,
     slidesToScroll:1,
+    responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
